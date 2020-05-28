f14f22097da19491974f529d83184abb7482b167
Lucas Machado <machadolucasvp@gmail.com>
2020-05-27 21:07:04 -0300

    feat: add product model and product repository

---
+++ src/repositories/impl/ProductRepository.ts
@@ -0,0 +1,73 @@
+import Repository from '../Repository.ts';
+import Product from '../../models/Product.ts';
+import Database from "../../database/database.ts";
+
+class ProductRepository implements Repository<number, Product> {
+
+  async getAll(): Promise<Product[]>{
+    const result = await Database.query("select * from products;");
+
+    const products = result.rowsOfObjects() as Product[];
+
+    return products;
+  };
+
+
+  async get(identifier: number): Promise<Product>{
+    const result = await Database.query({
+      text: `select id, name, price, barcode, description from products where id=$1;`,
+      args: [identifier],
+    });
+
+    return result.rowsOfObjects()[0] as Product;
+
+  };
+
+  async post(product: Product): Promise<Product>{
+    const result = await Database.query({
+      text:
+        "insert into products (id, name, price, barcode, description) values ($1, $2, $3, $4, $5) returning id, title, isbn;",
+      args: [
+        product.id,
+        product.name,
+        product.price,
+        product.barcode,
+        product.description,
+      ],
+    });
+
+    return result.rowsOfObjects()[0] as Product;
+
+  };
+
+  async put(product: Product): Promise<Product>{
+    const result = await Database.query({
+      text:
+      'update products set name=$2, price=$3, description=$4 where id=$1',
+      args: [
+        product.id,
+        product.name,
+        product.price,
+        product.description,
+      ]
+    })
+ 
+    return result.rowsOfObjects()[0] as Product;
+
+  };
+
+  async delete(identifier: number): Promise<Product>{
+    const result = await Database.query({
+      text:
+        "delete from products where id=$1 returning id, name, description;",
+      args: [
+        identifier
+      ],
+    });
+
+    return result.rowsOfObjects()[0] as Product;
+
+  };
+}
+
+export default new ProductRepository();
